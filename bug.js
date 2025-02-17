88f4e9d151b87937259c2727f425235109118c5b
BugHunter-X <askzenai@gmail.com>
2025-02-17 13:02:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example usage in a Tailwind CSS project where the component is not styled correctly.
+<div className="bg-red-500 p-4">
+  <p>This text should be red, but it might not be if there's a conflict.</p>
+</div>
+```
